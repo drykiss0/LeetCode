a740dcbef53a0d144438dba04053dcc36b01202a
Antoni Rasul <antoni.rasul@gmail.com>
2020-03-01 16:04:05 +0100

    Initial commit

---
+++ src/test/java/org/evoludev/AppTest.java
@@ -0,0 +1,20 @@
+package org.evoludev;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
