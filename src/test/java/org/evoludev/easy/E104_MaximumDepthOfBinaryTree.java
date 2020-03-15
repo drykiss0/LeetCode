88a6e35fd03340877acf49ed999922ed0479e537
Antoni Rasul <antoni.rasul@gmail.com>
2020-03-15 01:40:42 +0100

    E104 Maximum Depth of Binary Tree

---
+++ src/test/java/org/evoludev/easy/E104_MaximumDepthOfBinaryTree.java
@@ -0,0 +1,85 @@
+package org.evoludev.easy;
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Given a binary tree, find its maximum depth.
+ *
+ * The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+ *
+ * Note: A leaf is a node with no children.
+ *
+ * Example:
+ *
+ * Given binary tree [3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * return its depth = 3.
+ */
+public class E104_MaximumDepthOfBinaryTree {
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+
+        @Override
+        public String toString() {
+            return val + "";
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        return maxDepth(root, 1);
+    }
+
+    private int maxDepth(TreeNode node, int level) {
+        if (node == null) {
+            return level - 1;
+        }
+        return Math.max(maxDepth(node.left, level + 1), maxDepth(node.right, level + 1));
+    }
+
+    @Test
+    public void test() {
+
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        System.out.println(String.format("maxDepth(%s) = %s", toBfsString(root), maxDepth(root)));
+    }
+
+    public String toBfsString(TreeNode root) {
+        List<Integer> result = new LinkedList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.remove();
+            result.add(node.val);
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+        return result.toString();
+    }
+
+}
