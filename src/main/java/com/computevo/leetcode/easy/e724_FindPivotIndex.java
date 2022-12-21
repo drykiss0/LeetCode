5074e887de5a5ce2b3a2c96239a8bd691d54faec
Antoni Rasul <antoni.rasul-extern@dbschenker.com>
2022-12-21 22:22:21 +0100

    Better version of e724 find pivot index

--- src/main/java/com/computevo/leetcode/easy/e724_FindPivotIndex.java
+++ src/main/java/com/computevo/leetcode/easy/e724_FindPivotIndex.java
@@ -5,31 +5,24 @@ import java.util.Arrays;
 import static com.computevo.leetcode.easy.helper.Utils.printSolution;
 
 /**
- * https://leetcode.com/problems/find-pivot-index/?envType=study-plan&id=level-1
+ * <a href="https://leetcode.com/problems/find-pivot-index/?envType=study-plan&id=level-1">e724_FindPivotIndex</a>
  */
 public class e724_FindPivotIndex {
 
     public int pivotIndex(int[] nums) {
 
-        int[] runningSum = Arrays.copyOf(nums, nums.length);
-        for(int i = 1; i < runningSum.length; i++) {
-            runningSum[i] += runningSum[i-1];
-        }
-        int[] runningTailSum = Arrays.copyOf(nums, nums.length);
-        for(int i = runningTailSum.length - 1; i > 0; i--) {
-            runningTailSum[i-1] += runningTailSum[i];
-        }
-
-        for (int i = 0; i < runningSum.length; i++) {
-            if (runningSum[i] == runningTailSum[i])
-                return i;
+        int sum = Arrays.stream(nums).sum();
+        int leftSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (leftSum == sum - nums[i] - leftSum) return i;
+            leftSum += nums[i];
         }
         return -1;
     }
 
 
     public static void main(String[] args) {
-        int[] nums = {2,1,-1};
+        int[] nums = {-1,-1,0,1,1,0};
 
         int result = new e724_FindPivotIndex().pivotIndex(nums);
 
