90dc028e32f4cbd54137eaa2005ec410475c16d4
Antoni Rasul <antoni.rasul-extern@dbschenker.com>
2023-01-17 18:23:08 +0100

    LeetCode-75 Problems: 844. Backspace String Compare

---
+++ src/main/java/com/computevo/leetcode/e844_BackspaceStringCompare.java
@@ -0,0 +1,45 @@
+package com.computevo.leetcode;
+
+import java.util.LinkedList;
+
+import static com.computevo.leetcode.helper.Utils.printInput;
+import static com.computevo.leetcode.helper.Utils.printOutput;
+
+/**
+ * <a href="https://leetcode.com/problems/backspace-string-compare/?envType=study-plan&id=level-1">link</a>
+ */
+public class e844_BackspaceStringCompare {
+
+    public boolean backspaceCompare(String s, String t) {
+        return edited(s).equals(edited(t));
+    }
+
+    private LinkedList<Integer> edited(String s) {
+        LinkedList<Integer> stack = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != '#')
+                stack.push((int) s.charAt(i));
+            else if (!stack.isEmpty())
+                stack.pop();
+        }
+        return stack;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "ab#c";
+        String t1 = "ad#c";
+        String s2 = "ab##";
+        String t2 = "c#d#";
+        String s3 = "a#c";
+        String t3 = "b";
+
+        printInput("s", "t", new Object[][]{
+                {s1, t1}, {s2, t2}, {s3, t3}
+        });
+        printOutput(
+                "backspaceCompare(1)", new e844_BackspaceStringCompare().backspaceCompare(s1, t1),
+                "backspaceCompare(2)", new e844_BackspaceStringCompare().backspaceCompare(s2, t2),
+                "backspaceCompare(3)", new e844_BackspaceStringCompare().backspaceCompare(s3, t3)
+        );
+    }
+}
